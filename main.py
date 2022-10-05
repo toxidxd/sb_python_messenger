cf8e25c379c22d1c2663dd212acdaa94493fb7ae
toxidxd <toxid13@yandex.ru>
2022-10-05 13:53:07 +0300

    start flasking

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-def add_message(sender, text):
-    new_message = {
-        "sender": sender,
-        "text": text,
-        "time": "25:79" # TODO подставлять автоматом
-    }
+from flask import Flask
 
-    all_messages.append(new_message)
+app = Flask(__name__) # Настройки приложения
 
-
-def print_msg(message):
-    print(f"[{message['sender']}]: {message['text']} / {message['time']}")
+@app.route("/hello")
+def hello_page():
+    return "Wellcome to SkillMessenger"
 
 
-all_messages = []
+app.run()
 
-add_message("Misha", "Python is cool")
-add_message("pavel", "not cool")
-add_message("max", "Python is bad")
-add_message("kit", "hello")
+# def add_message(sender, text):
+#     new_message = {
+#         "sender": sender,
+#         "text": text,
+#         "time": "25:79" # TODO подставлять автоматом
+#     }
 
-# print_msg(all_messages[0])
+#     all_messages.append(new_message)
 
-for msg in all_messages:
-    print_msg(msg)
+
+# def print_msg(message):
+#     print(f"[{message['sender']}]: {message['text']} / {message['time']}")
+
+
+# all_messages = []
+
+# add_message("Misha", "Python is cool")
+# add_message("pavel", "not cool")
+# add_message("max", "Python is bad")
+# add_message("kit", "hello")
+
+# # print_msg(all_messages[0])
+
+# for msg in all_messages:
+#     print_msg(msg)
