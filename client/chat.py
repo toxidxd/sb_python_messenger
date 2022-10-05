9598e4dd6e30a3c81ae4701d19f60978da30e33d
toxidxd <toxid13@yandex.ru>
2022-10-05 16:07:36 +0300

    added send message button

--- client/chat.py
+++ client/chat.py
@@ -1,22 +1,29 @@
-import document as document
-
 from utils import set_timeout, fetch
 
 last_seen_id = 0
 send_message = document.getElementById("send_message")
+sender = document.getElementById("sender")
+message_text = document.getElementById("message_text")
 
-
+# Добавляет новое сообщение в список сообщений
 def append_message(message):
     pass
 
 
+# Вызывается при клике на send_message
 async def send_message_click(e):
-    pass
+    # Отправить запрос к странице /send_message
+    await fetch(f"/send_message?sender={sender.value}&text={message_text.value}", method="GET")
+    message_text.value = ""
 
 
+# Загружает новые сообщения с сервера и отображает их
 async def load_fresh_messages():
     pass
 
 
+# Устанавливаем действие при клике
 send_message.onclick = send_message_click
-set_timeout(2, load_fresh_messages)
+
+# Загружаем сообщения через 1 секунду
+# set_timeout(1, load_fresh_messages)
--- main.py
+++ main.py
@@ -22,10 +22,6 @@ def add_message(sender, text):
     all_messages.append(new_message)
 
 
-add_message("mike", "test1")
-add_message("john", "test2")
-
-
 # страница для получения списка сообщений
 @app.route("/get_messages")
 def get_messages():
@@ -46,29 +42,3 @@ def hello_page():
 
 
 app.run()
-
-# def add_message(sender, text):
-#     new_message = {
-#         "sender": sender,
-#         "text": text,
-#         "time": "25:79" # TODO подставлять автоматом
-#     }
-
-#     all_messages.append(new_message)
-
-
-# def print_msg(message):
-#     print(f"[{message['sender']}]: {message['text']} / {message['time']}")
-
-
-# all_messages = []
-
-# add_message("Misha", "Python is cool")
-# add_message("pavel", "not cool")
-# add_message("max", "Python is bad")
-# add_message("kit", "hello")
-
-# # print_msg(all_messages[0])
-
-# for msg in all_messages:
-#     print_msg(msg)
